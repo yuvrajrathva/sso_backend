48e24a2bdb4ef7d65b212c3245c0602f13e303c3
Rohit <kori.1@iitj.ac.in>
2024-09-05 15:44:07 +0530

    Hash password before saving to db
    Signed-off-by: Rohit <kori.1@iitj.ac.in>

--- app/crud.py
+++ app/crud.py
@@ -2,10 +2,12 @@ from sqlalchemy.orm import Session
 
 from app.models import User, ServiceProvider
 from app.schemas import UserSchema, ServiceProviderSchema
+from app.utils import hash_password
 
 
 def create_user(db: Session, user: UserSchema):
-    db_user = User(roll_no=user.roll_no, first_name=user.first_name, last_name=user.last_name, email=user.email, password=user.password, mobile_no=user.mobile_no, is_verified=user.is_verified)
+    hashed_password = hash_password(user.password)
+    db_user = User(roll_no=user.roll_no, first_name=user.first_name, last_name=user.last_name, email=user.email, password=hashed_password, mobile_no=user.mobile_no, is_verified=user.is_verified)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
