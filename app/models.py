7ecd7f532fdfc2898292af0ea24d52a2953f4b97
Yuvraj <rathva.1@iitj.ac.in>
2024-09-06 00:17:44 +0530

    Added phone, email and password validators

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from .database import Base
-
+import re
 
 class User(Base):
     __tablename__ = "users"
@@ -10,8 +10,28 @@ class User(Base):
     last_name = Column(String(20))
     email = Column(String(50), unique=True)
     password = Column(String(100))
-    mobile_no = Column(Integer)
+    phone_number = Column(String(10))
     is_verified = Column(Boolean)
+
+    def validate_email(self):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", self.email) or not re.match(r"^[^@]+@iitj\.ac\.in$", self.email):
+            raise ValueError("Invalid email format")
+    
+    def validate_phone_number(self):
+        if not re.match(r"^\+?1?\d{9,15}$", self.phone_number):
+            raise ValueError("Invalid phone number format")
+
+    def validate_password(self):
+        if len(self.password) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+        if not any(char.isdigit() for char in self.password):
+            raise ValueError("Password must contain at least one number")
+        if not any(char.isupper() for char in self.password):
+            raise ValueError("Password must contain at least one uppercase letter")
+        if not any(char.islower() for char in self.password):
+            raise ValueError("Password must contain at least one lowercase letter")
+        if not any(not char.isalnum() for char in self.password):
+            raise ValueError("Password must contain at least one special charater")
 
 
 class ServiceProvider(Base):
@@ -21,4 +41,3 @@ class ServiceProvider(Base):
     name = Column(String(100))
     redirect_url = Column(String(100))
     is_verified = Column(Boolean)
-
